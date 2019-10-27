02452aac2fa5c83fa05ac9db7c320739190a0064
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2019-10-27 17:41:53 +0530

    Give your own test case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from PIL import Image
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn.externals import joblib 
@@ -33,7 +34,11 @@ except ImportError:
 knn_from_joblib = joblib.load('filename.pkl')  
 
 X,y=mnist["data"], mnist["target"]
-resu=knn_from_joblib.predict([X[3000]])
+img = Image.open(input("Enter Image name")) #add your image to the current directory and then input when prompted
+im2arr = np.array(img)
+lol,lmao=im2arr.shape
+im2arr=im2arr.reshape(lol*lmao)
+resu=knn_from_joblib.predict([im2arr])
 print(resu)
 plot_digit(X[3000])
 save_fig("final.jpg")
